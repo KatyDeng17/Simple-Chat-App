01a6016e2eb1daab95cc36fe2735456c6305db10
Katy Deng <deng2017.xq@gmail.com>
2018-11-13 21:49:57 -0800

    setup mongodb with mlab and connect the app to mongodb via mongoose

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 //socket.io needs http server ; 
 const http = require('http'); 
 const httpServer = http.Server(app);
-const io = require('socket.io')(httpServer)
+const io = require('socket.io')(httpServer);
+//require mongoose
+const mongoose = require('mongoose')
 
 const bodyParser = require('body-parser');
 const port = 3000
@@ -15,6 +17,9 @@ app.use(express.static(__dirname))
 //using the body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
+//connecting to mlab 
+
+const dbUrl = 'mongodb://user:user12345@ds163013.mlab.com:63013/simple-chat-app'
 
 const messages = [
   { name: "katy", message: "hi" },
@@ -37,6 +42,10 @@ io.on('connection', (socket)=>{
   console.log('a user connected'); 
 })
 
+//connect to mongoose
+mongoose.connect(dbUrl,{ useNewUrlParser: true},(err)=>{
+  console.log('mongo db is connected', err)
+})
 const server = httpServer.listen(port,()=>{
   console.log(`server is running on ${port}`)
 }); 
